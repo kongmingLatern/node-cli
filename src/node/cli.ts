7e5031a5f4afed39891756cd420f432bd17865c8
凤之兮原 <1577310596@qq.com>
2023-08-03 10:03:45 +0800

    chore: update cli logic

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -8,56 +8,60 @@
     - 在打开的页面中，除了渲染依赖关系图外，期望对依赖关系做出初步分析，例如：
   	- 是否包含循环依赖；
   	- 同一个 package 是否包含多个版本实例；
- * 
+ *
  */
-import { cac } from 'cac'
-import fs from 'fs'
-const version = require('../package.json').version
+import { cac } from "cac";
+import { OpenWindow } from "../../scripts/cli";
+import { resolve } from "path";
+import * as fs from "fs";
 
-const cli = cac('node-cli').version(version).help()
+const packagePath = resolve(process.cwd(), "./package.json");
+
+const version = require(packagePath).version;
+
+const cli = cac("node-cli").version(version).help();
 
 cli
-	.command('analyze', 'analyze dependencies')
-	.option('--depth <depth>', 'Limit depth', {
-		default: null,
+  .command("analyze", "analyze dependencies")
+  .option("--depth <depth>", "Limit depth", {
+    default: null,
+  })
+  .option("--json <json>", "Limit depth", {
+    default: null,
+  })
+  .action(async ({ depth, json }) => {
+    // node-cli analyze --depth=3 --json 2
+    if (depth) {
+      const paths = getPackagePaths(process.cwd());
+      console.log("paths", paths);
 
-	})
-	.option('--json <json>', 'Limit depth', {
-		default: null,
-	})
-	.action(async ({ depth, json }) => {
-		// node-cli analyze --depth=3 --json 2
-		if (depth) {
-			   const paths= getPackagePaths(process.cwd())   
-
-			console.log('depth', depth) // Output: depth 3
-		}
-		if (json) {
-			console.log('json', json) // Output: json 2
-		}
-		if (!depth && !json) {
-			await OpenWindow('https://www.baidu.com')
-			console.log('1', 1)
-		}
-	})
-const getPackagePaths = (dir:any, filelist = []) => {
+      console.log("depth", depth); // Output: depth 3
+    }
+    if (json) {
+      console.log("json", json); // Output: json 2
+    }
+    if (!depth && !json) {
+      await OpenWindow("https://www.baidu.com");
+      console.log("1", 1);
+    }
+  });
+const getPackagePaths = (dir: string, filelist: string[] = []) => {
   const files = fs.readdirSync(dir);
-  
-  files.forEach(file => {
-    const filepath = dir + '/' + file;
+
+  files.forEach((file) => {
+    const filepath = dir + "/" + file;
     const stats = fs.statSync(filepath);
-    
+
     if (stats.isDirectory()) {
       filelist = getPackagePaths(filepath, filelist);
-    } else if (file === 'package.json') {
+    } else if (file === "package.json") {
       filelist.push(filepath);
     }
   });
-  
+
   return filelist;
 };
 
+cli.parse();
 
-cli.parse()
-
-export { cli }
+export { cli };
