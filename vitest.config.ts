eb6ba7e2b875d271da0d573f3034f6842ea55d14
凤之兮原 <1577310596@qq.com>
2023-08-04 08:41:07 +0800

    feat: add some dependencies and config resolve alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		globals: true,
-		environment: 'jsdom',
-	},
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@scripts": "/scripts",
+    },
+  },
+});
